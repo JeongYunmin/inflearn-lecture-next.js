9d9d8a2c5b2c4f7e06a1f2832cc47dd83d6a65ad
yuumni <minyunj77@gmail.com>
2024-02-28 14:53:09 +0900

    섹션 2 - header 구현 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,31 @@
+import { Fragment } from 'react';
+import Header from '@/components/common/Header';
+import styles from '../styles/header.module.scss';
+import Link from 'next/link';
+import { AiOutlineShareAlt } from 'react-icons/ai';
+import { VscFeedback } from 'react-icons/vsc';
+
 export default function Home() {
-  return <>Hello World!</>;
+  return (
+    <Fragment>
+      <Header
+        rightElements={[
+          <button
+            onClick={() => {
+              alert('복사 !');
+            }}
+            className={styles.box}
+            style={{ marginRight: 8 }}
+            key="button"
+          >
+            <AiOutlineShareAlt size={20} />
+          </button>,
+          <Link href="/feedback" className={styles.box} key="link">
+            <VscFeedback size={20} />
+          </Link>,
+        ]}
+      />
+      <main></main>
+    </Fragment>
+  );
 }
